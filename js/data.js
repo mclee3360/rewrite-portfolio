febad706d2ee0cb67d58e45c7f8b5513f989e4f4
mclee3360 <mclee3360@gmail.com>
2019-03-28 23:36:49 -0400

    Add content for All page

--- js/data.js
+++ js/data.js
@@ -11,6 +11,21 @@ $.ajax({
     {
         var entries = [];
         data.feed.entry.forEach(element => entries.push(extractData(element)));
+        entries.sort(sortEntries)
+        $("#table").DataTable({
+            data: entries,
+            columns: [
+                {title: "Title"},
+                {title: "Date Uploaded"},
+                {title: "Role"}
+            ],
+            order: [[1, "desc"]],
+            lengthChange: false,
+            dom: "ftpi",
+            scrollX: true,
+            pageLength: 25,
+            pagingType: "full"
+        });
     },
     error: function(data)
     {
@@ -19,18 +34,55 @@ $.ajax({
 });
 
 /**
- * Converts the JSON data into a clean object.
+ * Converts the JSON data into an array for the table.
  *
  * @param entry  the entry to convert from the JSON.
- * @return a cleaner object representation.
+ * @return an array of its attributes.
  */
 function extractData(entry)
 {
-    return data =
+    var name = entry.gsx$series.$t;
+    var url = entry.gsx$url.$t;
+    var link = "<a href=\"" + url + "\" target=\"_blank\">" + name + "</a>";
+    return [link, formatDate(entry.gsx$date.$t), entry.gsx$role.$t];
+}
+
+/**
+ * Formats the date strings in an easily sortable order (YYYY/MM/DD).
+ * Also returns "N/A" if no date.
+ *
+ * @param date  the date string from Google sheets.
+ * @return the formatted date.
+ */
+function formatDate(date)
+{
+    if (date.length > 0)
     {
-        date: entry.gsx$date.$t,
-        role: entry.gsx$role.$t,
-        series: entry.gsx$series.$t,
-        url: entry.gsx$url.$t
+        var values = date.split("/");
+        var day = values[1].length > 1 ? values[1] : "0" + values[1];
+        var month = values[0].length > 1 ? values[0] : "0" + values[0];
+        var year = values[2];
+        return year + "/" + month + "/" + day;
     }
+    return "N/A";
+}
+
+/**
+ * Sorts entries by date then title.
+ *
+ * @param e1  Entry 1 to be compared.
+ * @param e2  Entry 2 to be compared.
+ * @return whether greater, less than, or equal to.
+ */
+function sortEntries(e1, e2)
+{
+    if (e1[1] === e2[1])
+    {
+        if (e1[0] === e2[0])
+        {
+            return 0;
+        }
+        return e2[0] > e1[0] ? 1 : -1;
+    }
+    return e2[1] > e1[1] ? 1 : -1;
 }
